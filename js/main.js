54536c9926af0356b324d1653fcf589e1c813eb9
coshieldhelp <coshieldhelp@gmail.com>
2022-02-24 21:49:54 +0530

    new

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate =  new Date("March 25, 2022 15:37:25").getTime();
+        var finalDate =  new Date("June 03, 2022 15:37:25").getTime();
         //-date: "Mar 25 2021",
 
         $('.home-content__clock').countdown(finalDate)
